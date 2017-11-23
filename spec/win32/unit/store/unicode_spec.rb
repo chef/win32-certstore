f9b00bf9e32d238358e6449eb1c20845f79b636f
Vasu1105 <vasundhara.jagdale@msystechnologies.com>
2017-11-23 09:49:58 +0000

    Removed chef dependency from win32-certstore library
    Signed-off-by: Vasu1105 <vasundhara.jagdale@msystechnologies.com>

---
+++ spec/win32/unit/store/unicode_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'win32/certstore/mixin/unicode'
+
+describe Win32::Certstore::Mixin::Unicode do
+  context "when testing individual methods" do
+
+    describe "#read_wstring", :windows_only do
+      it "reads wide sring from Memory Pointer" do
+        po = FFI::MemoryPointer.new(1, 120)
+        po.write_string("()")
+        expect(po.read_wstring).to eql("\u2928")
+        po.clear
+        expect(po.read_wstring).to eql("")
+      end
+    end
+  end
+end
