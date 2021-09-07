c52c0fd44c31941efac39b1f61f547429fd9fc5c
rishichawda <rishichawda@users.noreply.github.com>
2021-09-07 18:37:27 +0530

    update tests
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- spec/win32/functional/win32/certstore_spec.rb
+++ spec/win32/functional/win32/certstore_spec.rb
@@ -39,9 +39,28 @@ RSpec.describe Win32::Certstore, :windows_only do
     end
 
     # passing invalid thumbprint
+    it "returns nil if certificate not found" do
+      thumbprint = "b1bc968bd4f49d622aa89a81f2150152a41d829cab"
+      cert_obj = @store.get(thumbprint, store_location: CERT_SYSTEM_STORE_CURRENT_USER, store_name: "My")
+      expect(cert_obj).to be_empty
+    end
+  end
+
+  describe "#get!" do
+    before { add_cert }
+    let(:cert_pem) { File.read('.\spec\win32\assets\GlobalSignRootCA.pem') }
+
+    # passing valid thumbprint
+    it "returns the certificate_object if found" do
+      thumbprint = "b1bc968bd4f49d622aa89a81f2150152a41d829c"
+      expect(@store).to receive(:cert_get).with(thumbprint, store_location: CERT_SYSTEM_STORE_CURRENT_USER, store_name: "My").and_return(cert_pem)
+      @store.get!(thumbprint, store_location: CERT_SYSTEM_STORE_CURRENT_USER, store_name: "My")
+    end
+
+    # passing invalid thumbprint
     it "returns ArgumentError if certificate not found" do
       thumbprint = "b1bc968bd4f49d622aa89a81f2150152a41d829cab"
-      expect { @store.get(thumbprint, store_location: CERT_SYSTEM_STORE_CURRENT_USER, store_name: "My") }.to raise_error(ArgumentError)
+      expect { @store.get!(thumbprint, store_location: CERT_SYSTEM_STORE_CURRENT_USER, store_name: "My") }.to raise_error(ArgumentError)
     end
   end
 
